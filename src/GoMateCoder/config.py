bb47d2031e6d757b7b9618164f4ee523ac59621b
LonelyReed <cxlxh220416@163.com>
2026-04-16 13:03:29 +0800

    重新跟踪文件

---
+++ src/GoMateCoder/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+# 定义一个setting类，继承BaseSettings
+
+# 旧有语法，依然支持有效，此处试用新版语法
+# class Settings(BaseSettings):
+#     llm_api_key: str = ""
+#     llm_name: str = "deepseek-chat"
+#     worksapce_root: str = "."
+#     command_timeout: int = 30
+
+#     class Config:
+#         env_file = ".env"
+#         env_file_encoding = "utf-8"
+
+class Settings(BaseSettings):
+    # 从环境变量加载配置
+    model_config = SettingsConfigDict(env_file=".env",env_file_encoding=".utf-8")
+
+    # 配置初始化，集中管理
+    llm_api_key: str = ""
+    llm_name:str = "deepseek-chat"
+    workspace_root:str = "."
+    command_timeout:int = 30
+
+
+settings = Settings()
