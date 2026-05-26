bf50d6ce3d4d22ac97db30af1c48ffb78a7e13d2
LonelyReed <cxlxh220416@163.com>
2026-05-26 22:32:58 +0800

    restart:init new structure

--- src/GoMateCoder/router.py
+++
@@ -1,9 +0,0 @@
-from .state import AgentState
-
-# 条件边判断
-def should_continue(state:AgentState) -> str:
-    """决定下一步应该进入哪一个节点"""
-    last_message = state["messages"][-1]
-    if hasattr(last_message,"tool_calls") and last_message.tool_calls:
-        return "tools"
-    return "end"
